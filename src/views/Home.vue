bdf74aad092c9b37bb9066130407e278051b39e7
Ansticefish <ansticefish0106@gmail.com>
2022-10-09 14:16:43 +0800

    Add color buttons for users to choose colors freely

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,36 +5,47 @@
     <img class="home__rain" v-if="rain" :src="rainDrop" alt="">
     <!-- Plants -->
     <div class="home__plants">
-      <img class="home__plants__one" v-if="currentStep > 0" src="../assets/plant1.png" alt="">
-      <img class="home__plants__two" v-if="currentStep > 1" src="../assets/plant2.png" alt="">
+      <img class="home__plants__one" v-if="currentStep > 0" :src="plants[0]" alt="">
+      <img class="home__plants__two" v-if="currentStep > 1" :src="plants[1]" alt="">
       <div class="home__plants__rabbit">
         <img class="home__plants__rabbit__one" 
         v-if="currentStep > 2" 
-        src="../assets/rabbit1.png" alt="">
+        :src="plants[2]" alt="">
         <img class="home__plants__rabbit__two" 
         v-if="currentStep > 3" 
-        src="../assets/rabbit2.png" alt="">
+        :src="plants[3]" alt="">
         <div class="home__plants__rabbit__head">
            <img class="home__plants__rabbit__head__face" 
            v-if="currentStep > 4" 
-           src="../assets/rabbit3.png" alt="">
+           :src="plants[4]" alt="">
           <img class="home__plants__rabbit__head__expression" 
           v-if="currentStep > 5" 
-          src="../assets/rabbit4.png" alt="">
+          :src="plants[5]" alt="">
         </div>  
         <img class="home__plants__rabbit__five" 
         v-if="currentStep > 6" 
-        src="../assets/rabbit5.png" alt="">
+        :src="plants[6]" alt="">
       </div>
     </div>
-    <img class="home__snail" @dblclick="callClouds" v-if="!processing"
+    <!-- Snail & Soil -->
+    <img class="home__snail" @dblclick="openButton" v-if="!processing"
     src="../assets/snail.png" alt="">
     <img class="home__soil" src="../assets/soil.png" alt="">
+    <!-- Instruction -->
     <div class="home__instruction" v-if="openInstruction">
       連續點擊蝸牛兩下，就可以召喚七彩雲朵囉！
       <div class="home__instruction__btn" @click="closeInstruction"> OK </div>
     </div>
-    <PopUp v-if="openPopUp" :step="currentStep" @close="togglePopUp"/>
+    <div class="home__btn" v-show="showBtn && !processing">
+      <div :class="['home__btn__red', {'clicked': colorUsed.red.length} ]" @click.once="callRain('red')"></div>
+      <div :class="['home__btn__orange', {'clicked': colorUsed.orange.length} ]" @click.once="callRain('orange')" ></div>
+      <div :class="['home__btn__yellow', {'clicked': colorUsed.yellow.length} ]" @click.once="callRain('yellow')" ></div>
+      <div :class="['home__btn__green', {'clicked': colorUsed.green.length} ]" @click.once="callRain('green')" ></div>
+      <div :class="['home__btn__blue', {'clicked': colorUsed.blue.length} ]" @click.once="callRain('blue')" ></div>
+      <div :class="['home__btn__lightb', {'clicked': colorUsed.lightb.length} ]" @click.once="callRain('lightb')" ></div>
+      <div :class="['home__btn__purple', {'clicked': colorUsed.purple.length} ]" @click.once="callRain('purple')" ></div>
+    </div>
+    <PopUp v-if="openPopUp" :step="currentStep" @close="togglePopUp" />
     <audio autoplay v-if="playMusic">
       <source :src="music" type="audio/mp3">
     </audio>
@@ -43,6 +54,98 @@
 
 <script>
 import PopUp from '../components/PopUp.vue'
+const data = [
+  {
+    red: require('../assets/plant1-red.png'),
+    orange: require('../assets/plant1-orange.png'),
+    yellow: require('../assets/plant1-yellow.png'),
+    green: require('../assets/plant1-green.png'),
+    blue: require('../assets/plant1-blue.png'),
+    lightb: require('../assets/plant1-lightb.png'),
+    purple: require('../assets/plant1-purple.png'),
+  },
+  {
+    red: require('../assets/plant2-red.png'),
+    orange: require('../assets/plant2-orange.png'),
+    yellow: require('../assets/plant2-yellow.png'),
+    green: require('../assets/plant2-green.png'),
+    blue: require('../assets/plant2-blue.png'),
+    lightb: require('../assets/plant2-lightb.png'),
+    purple: require('../assets/plant2-purple.png'),
+  },
+  {
+    red: require('../assets/rabbit1-red.png'),
+    orange: require('../assets/rabbit1-orange.png'),
+    yellow: require('../assets/rabbit1-yellow.png'),
+    green: require('../assets/rabbit1-green.png'),
+    blue: require('../assets/rabbit1-blue.png'),
+    lightb: require('../assets/rabbit1-lightb.png'),
+    purple: require('../assets/rabbit1-purple.png'),
+  },
+  {
+    red: require('../assets/rabbit2-red.png'),
+    orange: require('../assets/rabbit2-orange.png'),
+    yellow: require('../assets/rabbit2-yellow.png'),
+    green: require('../assets/rabbit2-green.png'),
+    blue: require('../assets/rabbit2-blue.png'),
+    lightb: require('../assets/rabbit2-lightb.png'),
+    purple: require('../assets/rabbit2-purple.png'),
+  },
+  {
+    red: require('../assets/rabbit3-red.png'),
+    orange: require('../assets/rabbit3-orange.png'),
+    yellow: require('../assets/rabbit3-yellow.png'),
+    green: require('../assets/rabbit3-green.png'),
+    blue: require('../assets/rabbit3-blue.png'),
+    lightb: require('../assets/rabbit3-lightb.png'),
+    purple: require('../assets/rabbit3-purple.png'),
+  },
+  {
+    red: require('../assets/rabbit4-red.png'),
+    orange: require('../assets/rabbit4-orange.png'),
+    yellow: require('../assets/rabbit4-yellow.png'),
+    green: require('../assets/rabbit4-green.png'),
+    blue: require('../assets/rabbit4-blue.png'),
+    lightb: require('../assets/rabbit4-lightb.png'),
+    purple: require('../assets/rabbit4-purple.png'),
+  }, 
+  {
+    red: require('../assets/rabbit5-red.png'),
+    orange: require('../assets/rabbit5-orange.png'),
+    yellow: require('../assets/rabbit5-yellow.png'),
+    green: require('../assets/rabbit5-green.png'),
+    blue: require('../assets/rabbit5-blue.png'),
+    lightb: require('../assets/rabbit5-lightb.png'),
+    purple: require('../assets/rabbit5-purple.png'),
+  },
+  {
+    red: require('../assets/cloud-red.png'),
+    orange: require('../assets/cloud-orange.png'),
+    yellow: require('../assets/cloud-yellow.png'),
+    green: require('../assets/cloud-green.png'),
+    blue: require('../assets/cloud-blue.png'),
+    lightb: require('../assets/cloud-lightb.png'),
+    purple: require('../assets/cloud-purple.png'),
+  },
+  {
+    red: require('../assets/rain-red.png'),
+    orange: require('../assets/rain-orange.png'),
+    yellow: require('../assets/rain-yellow.png'),
+    green: require('../assets/rain-green.png'),
+    blue: require('../assets/rain-blue.png'),
+    lightb: require('../assets/rain-lightb.png'),
+    purple: require('../assets/rain-purple.png'),
+  },
+  {
+    0: require('../assets/song-do.mp3'),
+    1: require('../assets/song-re.mp3'),
+    2: require('../assets/song-fa.mp3'),
+    3: require('../assets/song-me.mp3'),
+    4: require('../assets/song-so.mp3'),
+    5: require('../assets/song-la.mp3'),
+    6: require('../assets/song-si.mp3'),
+  },
+]
 
 export default {
   name: 'Home',
@@ -53,86 +156,35 @@ export default {
     return {
       currentStep: 0,
       rain: false,
+      plants: [ '', '', '', '', '', '', ''],
       cloud: 0,
       rainDrop: 0,
       music: 0,
+      showBtn: false,
       openInstruction: true,
       openPopUp: false,
       processing: false,
       playMusic: false,
+      colorUsed: {
+        'red': '', 'orange': '', 'yellow': '',
+        'green': '', 'blue': '', 'lightb': '', 'purple': ''
+      }
     }
   },
   methods: {
-    callClouds () {
-      this.processing = true
-      this.rain = true
-      setTimeout( () => {
-        this.currentStep += 1
-      }, 500 )
-      switch (this.currentStep) {
-        case 0:
-          this.cloud = require('../assets/cloud-blue.png')
-          this.rainDrop = require('../assets/rain-blue.png')
-          this.music = require('../assets/song-do.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-        case 1:
-          this.cloud = require('../assets/cloud-purple.png')
-          this.rainDrop = require('../assets/rain-purple.png')
-          this.music = require('../assets/song-re.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-        case 2:
-          this.cloud = require('../assets/cloud-red.png')
-          this.rainDrop = require('../assets/rain-red.png')
-          this.music = require('../assets/song-fa.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-        case 3:
-          this.cloud = require('../assets/cloud-orange.png')
-          this.rainDrop = require('../assets/rain-orange.png')
-          this.music = require('../assets/song-me.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-        case 4:
-          this.cloud = require('../assets/cloud-yellow.png')
-          this.rainDrop = require('../assets/rain-yellow.png')
-          this.music = require('../assets/song-so.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-        case 5:
-          this.cloud = require('../assets/cloud-green.png')
-          this.rainDrop = require('../assets/rain-green.png')
-          this.music = require('../assets/song-la.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-        case 6:
-          this.cloud = require('../assets/cloud-lightb.png')
-          this.rainDrop = require('../assets/rain-lightb.png')
-          this.music = require('../assets/song-si.mp3')
-          this.playMusic = true
-          this.stopRain()
-          break;
-      }
+    closeInstruction() {
+      this.openInstruction = false
     },
-    stopRain () {
-      setTimeout(() => {
-        this.rain = false
-        this.openPopUp = !this.openPopUp
-        this.processing = false
-      }, 3000)  
+    openButton () {
+      this.showBtn = !this.showBtn
     },
     togglePopUp () {
       this.openPopUp = !this.openPopUp
       this.playMusic = false 
+      this.processing = false
       if (this.currentStep === 7) {
         this.rain = true
+        this.rainDrop = ''
         this.cloud = require('../assets/cloud-final.png')
         setTimeout(() => {
           this.music = require('../assets/song-final.mp3')
@@ -141,9 +193,26 @@ export default {
         this.processing = true
       }
     },
-    closeInstruction () {
-      this.openInstruction = false
-    }
+    callRain(color) {
+      this.processing = true
+      this.rain = true
+      setTimeout(() => {
+        this.currentStep += 1
+      }, 500)
+      this.cloud = data[7][color]
+      this.rainDrop = data[8][color]
+      this.colorUsed[color] = 'true'
+      this.plants[this.currentStep] = data[this.currentStep][color]
+      this.music = data[9][this.currentStep]
+      this.playMusic = true
+      this.stopRain()
+    },
+    stopRain() {
+      setTimeout(() => {
+        this.rain = false
+        this.openPopUp = !this.openPopUp
+      }, 3000)
+    },
   }
 }
 </script>
@@ -245,6 +314,62 @@ export default {
       cursor: pointer;
       color: white;
       font-size: 25px;
+      &:hover {
+        opacity: 0.5;
+        transition: opacity 1s;
+      }
+    }
+  }
+  &__btn {
+    position: absolute;
+    bottom: 20%;
+    right: 3%;
+    border: 3px solid rgb(224, 158, 158);
+    border-radius: 15px;
+    background-color: rgb(240, 220, 220);
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    grid-template-rows: repeat(4, auto);
+    grid-gap: 8px; 
+    padding: 8px;
+    & div {
+      width: 10vw;
+      height: 10vw;
+      max-width: 100px;
+      max-height: 100px;
+      background-color: rgba(204, 170, 170, 0.2);
+      border-radius: 15px;
+      background-size: cover;
+      cursor: pointer;
+      &:hover {
+        opacity: 0.2;
+        transition: opacity 0.5s;
+      }
+      &.clicked {
+        opacity: 0.15;
+        cursor: default;
+      }
+    }
+    &__red {
+      background-image: url('../assets/btn-red.png');
+    }
+    &__orange {
+      background-image: url('../assets/btn-orange.png');
+    }
+    &__yellow {
+      background-image: url('../assets/btn-yellow.png');
+    }
+    &__green {
+      background-image: url('../assets/btn-green.png');
+    }
+    &__blue {
+      background-image: url('../assets/btn-blue.png');
+    }
+    &__lightb {
+      background-image: url('../assets/btn-lightb.png');
+    }
+    &__purple {
+      background-image: url('../assets/btn-purple.png');
     }
   }
 }
